4beb7c3bd7cd7f8171573410efdd9fbc8c4578f0
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-12-14 08:22:57 -0300

    criado tabela do banco de dados e as rotas de listagens e de criação de notifiação

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from "node:crypto";
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller("notifications")
 export class AppController {
-  constructor() { }
+  constructor(private readonly prisma: PrismaService) { }
 
   @Get()
-  getHello(): string {
-    return "Hello World";
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      }
+    });
   }
 }
